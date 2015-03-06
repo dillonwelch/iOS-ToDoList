134356b4278012e330e66ca8cbad56cd990c2716
Dillon Welch <daw0328@gmail.com>
2015-03-06 15:02:49 -0800

    Initialize a mock NSCoder and a ToDoItem.

--- ToDoListTests/ToDoItemCase.m
+++ ToDoListTests/ToDoItemCase.m
@@ -16,27 +16,25 @@
 
 @implementation ToDoItemCase
 
+id nsCoderMock;
+
+ToDoItem *item1;
+
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
+    nsCoderMock = OCMClassMock([NSCoder class]);
+    item1 = [[ToDoItem alloc] init];
+    item1.itemName = @"Pet the cat.";
+    item1.completed = NO;
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
